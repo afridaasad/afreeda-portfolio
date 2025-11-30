40e139c268195c02130cb4e77bfdd334f320afce
afridaasad <afreedaasad.1808@gmail.com>
2025-11-30 19:58:36 +0530

    added experience

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import About from './components/About';
+import Experience from './components/Experience';
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Skills />
+      <Experience />
       
 
     </>
