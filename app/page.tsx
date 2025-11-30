0fc0381a501de47ff2dbaab529f45a8b07bd80ce
afridaasad <afreedaasad.1808@gmail.com>
2025-11-30 21:57:24 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 
-import Hero from './components/Hero';
-import Skills from './components/Skills';
-import About from './components/About';
-import Experience from './components/Experience';
+import Hero from "@/app/components/Hero";
+import Skills from "@/app/components/Skills";
+import About from "@/app/components/About";
+import Experience from "@/app/components/Experience";
+
 
 
 export default function Home() {
