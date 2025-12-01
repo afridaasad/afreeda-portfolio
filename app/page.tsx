4f9507ccf6ac739fafedfb96dd7bd4ad3d0b8623
afridaasad <afreedaasad.1808@gmail.com>
2025-12-01 14:10:02 +0530

    path change

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 
-import Hero from "@/app/components/Hero";
-import Skills from "@/app/components/Skills";
-import About from "@/app/components/About";
-import Experience from "@/app/components/Experience";
+import Hero from "@components/Hero";
+import About from "@components/About";
+import Skills from "@components/Skills";
+import Experience from "@components/Experience";
+
 
 
 
