15b18747e7b23a5411bb37065369196d3c7a9c19
afridaasad <afreedaasad.1808@gmail.com>
2025-12-01 20:13:51 +0530

    Fix import paths for Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import dynamic from "next/dynamic";
 
-const Hero = dynamic(() => import("@components/Hero"), { ssr: true });
-const About = dynamic(() => import("@components/About"), { ssr: true });
-const Skills = dynamic(() => import("@components/Skills"), { ssr: true });
-const Experience = dynamic(() => import("@components/Experience"), { ssr: true });
+const Hero = dynamic(() => import("./components/Hero"), { ssr: true });
+const About = dynamic(() => import("./components/About"), { ssr: true });
+const Skills = dynamic(() => import("./components/Skills"), { ssr: true });
+const Experience = dynamic(() => import("./components/Experience"), { ssr: true });
+
 
 export default function Home() {
   return (
