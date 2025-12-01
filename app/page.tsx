6dffeb899d0660ce16779c52c42710229256305d
afridaasad <afreedaasad.1808@gmail.com>
2025-12-01 19:36:35 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Header from "./components/Header";
-
-
-
-
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
 
 
 export default function Home() {
