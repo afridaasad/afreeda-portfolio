07ed1752dbf636e7b06a66e45704b72c29f0fea9
afridaasad <afreedaasad.1808@gmail.com>
2025-12-01 20:01:27 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 import dynamic from "next/dynamic";
 
-const Hero = dynamic(() => import("@/components/Hero"), { ssr: true });
-const About = dynamic(() => import("@/components/About"), { ssr: true });
-const Skills = dynamic(() => import("@/components/Skills"), { ssr: true });
-const Experience = dynamic(() => import("@/components/Experience"), { ssr: true });
-const Header = dynamic(() => import("@/components/Header"), { ssr: true });
-
-
+const Hero = dynamic(() => import("@components/Hero"), { ssr: true });
+const About = dynamic(() => import("@components/About"), { ssr: true });
+const Skills = dynamic(() => import("@components/Skills"), { ssr: true });
+const Experience = dynamic(() => import("@components/Experience"), { ssr: true });
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <About />
       <Skills />
       <Experience />
-      
-
     </>
   );
 }
